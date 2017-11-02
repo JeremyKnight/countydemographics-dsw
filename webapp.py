659c176a759d96ac4ee0c21d1d9491fcab375b35
jaredfitton <jaredfitton@gmail.com>
2017-11-02 09:45:02 -0700

    First Test

--- webapp.py
+++ webapp.py
@@ -6,11 +6,21 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 
 @app.route("/")
 def render_main():
-    return render_template('index.html')
-
+    with open('county_demographics.json') as demographics_data:
+        counties = json.load(demographics_data)
+    return render_template('index.html', response = get_state_options)
 # @app.route("/before")
 # def render_page1():
 #     return render_template('before.html')
+
+def get_state_options:
+    states = []
+    options = ""
+    for c in counties:
+        if c["State"] not in states:
+            states.append(c["State"])
+            options += Markup("<option value=\"" + c["State"] + "\">" + c["State"] + "</option>")
+    return options
 
 if __name__=="__main__":
     app.run(debug=False, port=54321)

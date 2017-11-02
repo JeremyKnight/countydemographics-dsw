548b01d89b128f70c413e5578cc1b00b721eca13
jaredfitton <jaredfitton@gmail.com>
2017-11-02 10:15:31 -0700

    Added state info

--- webapp.py
+++ webapp.py
@@ -8,10 +8,10 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 def render_main():
     with open('static/county_demographics.json') as demographics_data:
         counties = json.load(demographics_data)
-    return render_template('index.html', response = get_state_options(counties))
-# @app.route("/before")
-# def render_page1():
-#     return render_template('before.html')
+    if 'State' in request.args:
+        selected_state = request.args["State"]
+        return render_template('index.html', response_options = get_state_options(counties), response_population = get_2014_population(counties, selected_state))
+    return render_template('index.html', response_options = get_state_options(counties))
 
 def get_state_options(counties):
     states = []
@@ -21,6 +21,13 @@ def get_state_options(counties):
             states.append(c["State"])
             options += Markup("<option value=\"" + c["State"] + "\">" + c["State"] + "</option>")
     return options
+
+def get_2014_population(counties, selected_state):
+    total_population = 0
+    for c in counties:
+        if c["State"] == selected_state:
+            total_population += c["Population"]["2014 Population"]
+    return str(total_population)
 
 if __name__=="__main__":
     app.run(debug=False, port=54321)

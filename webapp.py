6ac29046c18eaf1867834bdc50684197b0576955
jaredfitton <jaredfitton@gmail.com>
2017-11-02 10:17:44 -0700

    Added state to response

--- webapp.py
+++ webapp.py
@@ -10,7 +10,7 @@ def render_main():
         counties = json.load(demographics_data)
     if 'State' in request.args:
         selected_state = request.args["State"]
-        return render_template('index.html', response_options = get_state_options(counties), response_population = get_2014_population(counties, selected_state))
+        return render_template('index.html', response_options = get_state_options(counties), response_population = get_2014_population(counties, selected_state), response_state = selected_state)
     return render_template('index.html', response_options = get_state_options(counties))
 
 def get_state_options(counties):
